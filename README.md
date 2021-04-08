9322eb0962fe782bdd3f8d10f8199552e34afcbe
Matteson Grossman <mattesongrossman@gmail.com>
2021-04-08 10:48:34 -0400

    repo change

--- README.md
+++ README.md
@@ -1,31 +1,35 @@
 # Project-2---Fantasy-Site
 
+## Depreciated - Not updated since 2018
+
+## Hosted on https://murmuring-river-20812.herokuapp.com/
+
 APPROACH TAKEN:
 
-*   Site created as a tool for fantasy baseball owners.
-    *   Tool offers a more intuitive way to help with the draft and management of an owners team.
+- Site created as a tool for fantasy baseball owners.
+  - Tool offers a more intuitive way to help with the draft and management of an owners team.
 
 TECHNOLOGIES USED:
 
-*   PostgreSQL for Database integration
+- PostgreSQL for Database integration
 
--   NPMS used:
-    *   express
-    *   pg-promise
-    -   body-parser
-    -   method-override
+* NPMS used:
+  - express
+  - pg-promise
+  * body-parser
+  * method-override
 
-*   Bootstrap Template
-    *   https://startbootstrap.com/template-overviews/sb-admin-2/
+- Bootstrap Template
+  - https://startbootstrap.com/template-overviews/sb-admin-2/
 
 INSTALLATION INSTRUCTIONS
 
 NPM:
 
-*   npm install express
-*   npm install body-parser
-*   npm install pg-promise
-*   npm install method-override
+- npm install express
+- npm install body-parser
+- npm install pg-promise
+- npm install method-override
 
 Bootstrap source:
 
@@ -33,11 +37,11 @@ Forked From Github : https://github.com/BlackrockDigital/startbootstrap-sb-admin
 
 FEATURE LIST:
 
-*   Ranked list of players (pitchers/batters)
-*   Know players who were drafted
-*   List of all players by adp (average draft position)
-*   Sort stats by column
-*   Option to search for players
-*   Option to track what players are on my team
-*   Edit my team
-*   Player news and updates
+- Ranked list of players (pitchers/batters)
+- Know players who were drafted
+- List of all players by adp (average draft position)
+- Sort stats by column
+- Option to search for players
+- Option to track what players are on my team
+- Edit my team
+- Player news and updates
--- server.js
+++ server.js
@@ -1,44 +1,40 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 4567;
-const bodyParser = require("body-parser");
-const players = require("./models/fantasy_central");
-const news = require("./models/index");
-const fetch = require("node-fetch");
-const moment = require("moment");
-const findAll = players.findAll;
-const findAllPitchers = players.findAllPitchers;
-const batterDraftStatus = players.batterDraftStatus;
-const pitcherDraftStatus = players.pitcherDraftStatus;
-const getNews = news.getNews;
-const findBatterById = players.findBatterById;
-const getMyTeam = players.getMyTeam;
-const addPlayer = players.addPlayer;
-const deletePlayer = players.deletePlayer;
+const express = require("express")
+const app = express()
+const PORT = 3000
+const bodyParser = require("body-parser")
+const players = require("./models/fantasy_central")
+const news = require("./models/index")
+const fetch = require("node-fetch")
+const moment = require("moment")
+const findAll = players.findAll
+const findAllPitchers = players.findAllPitchers
+const batterDraftStatus = players.batterDraftStatus
+const pitcherDraftStatus = players.pitcherDraftStatus
+const getNews = news.getNews
+const findBatterById = players.findBatterById
+const getMyTeam = players.getMyTeam
+const addPlayer = players.addPlayer
+const deletePlayer = players.deletePlayer
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const methodOverride = require("method-override");
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const methodOverride = require("method-override")
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
 
-app.use(bodyParser.json());
-app.use(express.static("assets"));
-app.use(methodOverride("_method"));
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+app.use(bodyParser.json())
+app.use(express.static("assets"))
+app.use(methodOverride("_method"))
 
 //GET INDEX PAGE ON API CALL
 app.get("/", (request, response) => {
   let link =
-    "https://newsapi.org/v2/top-headlines?sources=espn&apiKey=596765967a80416c8c7f60c9fc7cba2a";
+    "https://newsapi.org/v2/top-headlines?sources=espn&apiKey=596765967a80416c8c7f60c9fc7cba2a"
   let get = url => {
-    return fetch(url).then(response => response.json());
-  };
+    return fetch(url).then(response => response.json())
+  }
   let displayNews = get(link).then(response => {
-    return response;
-  });
+    return response
+  })
   displayNews.then(newsData => {
     response.render(
       "index",
@@ -46,36 +42,36 @@ app.get("/", (request, response) => {
       // {
       //   newsDate: moment().format(newsData.articles[0].publishedAt)
       // }
-    );
-  });
-});
+    )
+  })
+})
 
 //GET FANTASY CENTRAL PAGE - BATTERS
 app.get("/fantasy_central", (request, response) => {
   findAll().then(playerData => {
-    response.render("fantasy_central", { playerData: playerData });
-  });
-});
+    response.render("fantasy_central", { playerData: playerData })
+  })
+})
 
 app.post("/fantasy_central", (request, response) => {
   findAll().then(playerData => {
-    response.render("fantasy_central", { playerData: playerData });
-  });
-});
+    response.render("fantasy_central", { playerData: playerData })
+  })
+})
 
 //GET FANTASY CENTRAL PAGE - PITCHERS
 app.get("/fantasy_central_pitchers", (request, response) => {
   findAllPitchers().then(playerData => {
-    response.render("fantasy_central_pitchers", { playerData: playerData });
-  });
-});
+    response.render("fantasy_central_pitchers", { playerData: playerData })
+  })
+})
 
 //GET MY TEAM PAGE
 app.get("/myTeam", (request, response) => {
   getMyTeam().then(playerData => {
-    response.render("myTeam", { playerData: playerData });
-  });
-});
+    response.render("myTeam", { playerData: playerData })
+  })
+})
 
 //ADD PLAYER TO TEAM DATABASE
 app.post("/myTeam", urlencodedParser, (request, response) => {
@@ -84,43 +80,47 @@ app.post("/myTeam", urlencodedParser, (request, response) => {
   // const playerId = Number(request.body.playerid);
   addPlayer(request.body)
     .then(data => {
-      response.redirect(`/myTeam`);
+      response.redirect(`/myTeam`)
     })
     .catch(error => {
-      response.send("Player already added to your team!");
-    });
-});
+      response.send("Player already added to your team!")
+    })
+})
 
 //EDIT DRAFT STATUS - BATTERS
 app.put("/fantasy_central/:id", urlencodedParser, (request, response) => {
-  const playerId = request.params.id;
-  const draftStatus = request.body.drafted;
-  batterDraftStatus(playerId, draftStatus);
-  response.redirect(`/fantasy_central`);
-});
+  const playerId = request.params.id
+  const draftStatus = request.body.drafted
+  batterDraftStatus(playerId, draftStatus)
+  response.redirect(`/fantasy_central`)
+})
 
 //EDIT DRAFT STATUS - PITCHERS
 app.put(
   "/fantasy_central_pitchers/:id",
   urlencodedParser,
   (request, response) => {
-    const playerId = request.params.id;
-    const draftStatus = request.body.drafted;
-    pitcherDraftStatus(playerId, draftStatus);
-    response.redirect(`/fantasy_central_pitchers`);
+    const playerId = request.params.id
+    const draftStatus = request.body.drafted
+    pitcherDraftStatus(playerId, draftStatus)
+    response.redirect(`/fantasy_central_pitchers`)
   }
-);
+)
 
 //DELETE PLAYER FROM TEAM
 app.delete("/myTeam/:id", (request, response) => {
-  const playerId = request.params.id;
-  deletePlayer(playerId);
-  response.redirect(`/myTeam`);
-});
+  const playerId = request.params.id
+  deletePlayer(playerId)
+  response.redirect(`/myTeam`)
+})
 
 //LINKS PAGE
 app.get("/links", (request, response) => {
   findAll().then(playerData => {
-    response.render("links", { playerData: playerData });
-  });
-});
+    response.render("links", { playerData: playerData })
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
